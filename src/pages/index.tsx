bd74aab2938ed38f0647d8c08685b454c6082871
Boris Rossovsky <boris.rozzy@gmail.com>
2022-05-11 00:52:48 -0400

    Beginnings of frontend

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,15 @@
 import type { NextPage } from "next"
+import Header from "../components/Header"
+import Input from "../components/Input"
+import Wrapper from "../components/Wrapper"
 
 const Home: NextPage = () => {
-  return <div>Home page </div>
+  return (
+    <Wrapper>
+      <Header />
+      {/* <Input labelText="LabelText" /> */}
+    </Wrapper>
+  )
 }
 
 export default Home
