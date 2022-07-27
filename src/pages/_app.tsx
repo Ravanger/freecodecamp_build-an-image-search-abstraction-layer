b8c737db1f3cdc4687dafa48a7dc9df70cacfbd3
Boris Rossovsky <boris.rozzy@gmail.com>
2022-07-27 02:42:39 -0400

    Reset.css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import type { AppProps } from "next/app"
-import "../style/global.css"
+import "../style/global.scss"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
