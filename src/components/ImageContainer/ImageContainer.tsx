330f246d48f89a49435d0a563a86bc28a68a9b52
Boris Rossovsky <boris.rozzy@gmail.com>
2022-08-09 01:48:50 -0400

    Dynamic grid + Loading spinner for images

--- src/components/ImageContainer/ImageContainer.tsx
+++ src/components/ImageContainer/ImageContainer.tsx
@@ -1,19 +1,34 @@
 import Image from "next/image"
 import { ImageContainerPropTypes } from "./ImageContainer.types"
 import styles from "./ImageContainer.module.scss"
+import { useState } from "react"
+import Spinner from "../Spinner"
 
 const ImageContainer: React.FC<ImageContainerPropTypes> = ({ imageData }) => {
+  const [imageLoading, setImageLoading] = useState(true)
+
   return (
     <div className={styles.imageWrapper}>
-      <span>{"..."}</span>
+      {imageLoading && (
+        <div className={styles.loader}>
+          <Spinner />
+        </div>
+      )}
       <a href={imageData.parentPage} target="_blank" rel="noreferrer">
-        <Image
-          src={imageData.thumbnail.url}
-          width={imageData.thumbnail.width}
-          height={imageData.thumbnail.height}
-          alt={imageData.description}
-          quality={100}
-        />
+        <div className={styles.imageCard}>
+          <Image
+            src={imageData.thumbnail.url}
+            width={imageData.thumbnail.width}
+            height={imageData.thumbnail.height}
+            alt={imageData.description}
+            quality={100}
+            objectFit="cover"
+            onLoadingComplete={() => {
+              setImageLoading(false)
+            }}
+            className={styles.image}
+          />
+        </div>
       </a>
     </div>
   )
--- src/components/ImageGrid/ImageGrid.tsx
+++ src/components/ImageGrid/ImageGrid.tsx
@@ -1,9 +1,10 @@
 import ImageContainer from "../ImageContainer"
 import { ImageGridPropTypes } from "./ImageGrid.types"
+import styles from "./ImageGrid.module.scss"
 
 const ImageGrid: React.FC<ImageGridPropTypes> = ({ images }) => {
   return (
-    <div>
+    <div className={styles.imageGrid}>
       {images.map((imageData) => (
         <ImageContainer imageData={imageData} key={imageData.parentPage} />
       ))}
